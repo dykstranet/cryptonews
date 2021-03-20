7299b75fc19a61e3e1679b4cfa3c3981b3a6e9c8
rht <rhtbot@protonmail.com>
2021-03-19 21:14:29 -0400

    Add script to generate annotated_price_data

---
+++ scripts/generate_annotated_price_data.py
@@ -0,0 +1,35 @@
+import csv
+import datetime
+
+import investpy
+
+today = datetime.datetime.today()
+today_str = today.strftime('%d/%m/%Y')
+a_year_ago = today - datetime.timedelta(days=365)
+a_year_ago_str = a_year_ago.strftime('%d/%m/%Y')
+
+data = investpy.get_crypto_historical_data(
+    crypto='bitcoin',
+    from_date=a_year_ago_str,
+    to_date=today_str)
+
+data.to_csv('data/1year_btcusd.csv')
+
+with open('data/1year_btcusd_annotated.csv', 'w') as f1:
+    annotated = csv.writer(f1)
+    f2 = open('data/1year_btcusd.csv', 'r')
+    btcusd = csv.reader(f2)
+
+    head = next(btcusd)
+    annotated.writerow(head + ['News'])
+
+    news = {}
+    with open('data/2021_news.csv', 'r') as f3:
+        csv_news = csv.reader(f3)
+        for row in csv_news:
+            news[row[0]] = row[2]
+
+    for row in btcusd:
+        annotated_row = row + [news.get(row[0], "")]
+        annotated.writerow(annotated_row)
+    f2.close()
