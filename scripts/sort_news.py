84905975aaab3ad35a491bf3b72dfb6d70feecc5
rht <rhtbot@protonmail.com>
2021-10-12 07:18:28 -0400

    sort_news: Detect when a line in news.csv is empty

--- scripts/sort_news.py
+++ scripts/sort_news.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python3
-import csv, datetime
+import csv
+import datetime
 
 rows = []
 
 with open('data/news.csv', 'r') as f:
     reader = csv.reader(f)
     for row in reader:
+        if row == []:
+            raise Exception("Error: one of the rows is empty.")
         rows.append(row)
 
 def get_row_key(row):
     try:
         return datetime.datetime.strptime(row[0], "%Y-%m-%d")
     except Exception:
-        print("Error: Your date is misformatted:", row[0])
+        print("Error: Your date is misformatted:", row)
         exit(1)
 
 rows.sort(key=get_row_key)
