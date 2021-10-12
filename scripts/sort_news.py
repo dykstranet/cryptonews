9c1c4a15a568785efb0ed937c6f1c16b6ce0fe7e
rht <rhtbot@protonmail.com>
2021-10-12 07:12:34 -0400

    sort_news: Print more info when exception happens

--- scripts/sort_news.py
+++ scripts/sort_news.py
@@ -10,9 +10,9 @@ with open('data/news.csv', 'r') as f:
 
 def get_row_key(row):
     try:
-        return datetime.datetime.strptime(row[0],"%Y-%m-%d")
+        return datetime.datetime.strptime(row[0], "%Y-%m-%d")
     except Exception:
-        print("Error: Your date is misformatted")
+        print("Error: Your date is misformatted:", row[0])
         exit(1)
 
 rows.sort(key=get_row_key)
